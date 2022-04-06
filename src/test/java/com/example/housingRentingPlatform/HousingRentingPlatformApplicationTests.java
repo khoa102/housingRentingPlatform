d06f4d12ac6ac9b46f35ef47edbe7c0e962d93d0
Khoa Tran <khoa.tran@incepit.com>
2022-04-06 17:30:01 +0700

    first commit

---
+++ src/test/java/com/example/housingRentingPlatform/HousingRentingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.housingRentingPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingRentingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
